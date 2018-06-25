36eb7dc1e0b75b11cacd90b03d2e00fa7863cf3a
Danny Wilson <dwilson@getchalk.com>
2018-06-25 18:53:22 -0300

    Better caching

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,15 @@ module.exports = {
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     {
+      resolve: `gatsby-plugin-netlify`,
+      options: {
+        headers: {
+          '/*': [`Cache-Control: max-age=31536000`],
+          '/*.html': [`Cache-Control: max-age=0, must-revalidate, public`]
+        }
+      }
+    },
+    {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'images',
